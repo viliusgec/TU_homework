bd5fd6f051b3f599f7076406a6282fd1ef8b9034
Vilius Gecas <55325682+viliusgec@users.noreply.github.com>
2020-11-02 13:53:55 +0200

    Add files via upload

---
+++ TU_homework/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace TU_homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int shift = getShift();
+            string text = getText();
+            _CaesarCipher cipher = new _CaesarCipher();
+            string encryptedText = cipher.encryptText(text, shift);
+            string decryptedText = cipher.decryptText(encryptedText, shift);
+            Console.WriteLine($"Encrypted text: {encryptedText}");
+            Console.WriteLine($"Decypted text: {decryptedText}");
+        }
+
+        public static int getShift()
+        {
+            Console.Write("Enter shift: ");
+            int shift;
+            while (true)
+            {
+                try
+                {
+                    shift = Int32.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Shift must be an integer number.");
+                    Console.Write("Please enter shift again: ");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("The number cannot fit in an Int32.");
+                }
+            }
+            return shift;
+        }
+
+        public static string getText()
+        {
+            string text = null;
+            while (string.IsNullOrEmpty(text))
+            {
+                Console.Write("Enter text: ");
+                text = Console.ReadLine();
+            }
+            return text;
+        }
+    }
+}
---
+++ TU_homework/﻿CaesarCipher.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+
+namespace TU_homework
+{
+    public class _﻿CaesarCipher
+    {
+        /// <summary>
+        /// Returns encrypted text
+        /// </summary>
+        /// <param name="text">Text that will be encrypted</param>
+        /// <param name="shift">Shift used to encrypt</param>
+        /// <returns>Encrypted text</returns>
+        public string encryptText(string text, int shift)
+        {
+            StringBuilder encryptedText = new StringBuilder();
+            //Changes shift value if current value is negative
+            shift = shift >= 0 ? shift : (26-Math.Abs(shift % 26));
+            char shiftedLetter;
+            foreach(char letter in text)
+            {
+                if(char.IsLetter(letter))
+                {
+                    //Using to determine if letter is capital or small
+                    char startIndex = char.IsUpper(letter) ? 'A' : 'a';
+                    //Using ASCII. shifted_letter = ((letter_code + shift) - start_index) mod (alphabet_lenght) + start_index
+                    shiftedLetter = (char)(((letter + shift) - startIndex) % 26 + startIndex);
+                }
+                else
+                {
+                    shiftedLetter = letter;
+                }
+                encryptedText.Append(shiftedLetter);
+            }
+            return encryptedText.ToString();
+        }
+
+        /// <summary>
+        /// Returns decrypted text
+        /// </summary>
+        /// <param name="text">Text that will be decrypted</param>
+        /// <param name="shift">Shift used to decrypt</param>
+        /// <returns>Decrypted text</returns>
+        public string decryptText(string text, int shift)
+        {
+            return encryptText(text, 26 - shift);
+        }
+    }
+}
